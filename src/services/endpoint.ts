7855f4124be2558999f2a8a58d798a1da4675359
itpohgero <itpohgero@gmail.com>
2024-07-09 12:24:42 +0700

    Feat: Add endpoint get contact with srw

---
+++ src/services/endpoint.ts
@@ -0,0 +1,3 @@
+export const Endpoint = {
+    contact_list: "/contact"
+}
--- src/services/useData.ts
+++ src/services/useData.ts
@@ -1,26 +1,38 @@
 import axios, { type AxiosRequestConfig } from "axios";
 import useSWR from "swr";
 
+const axiosInstance = axios.create({
+	baseURL: "https://contact.herokuapp.com",
+	headers: {
+		"Content-Type": "application/json",
+	},
+});
+
 interface FetchOptions extends AxiosRequestConfig {}
+
+const fetcher = async <T>(
+	url: string,
+	options: FetchOptions = {},
+): Promise<T> => {
+	const response = await axiosInstance({
+		url,
+		method: options.method || "GET",
+		headers: {
+			"Content-Type": "application/json",
+			...options.headers,
+		},
+		data: options.data,
+	});
+	return response.data;
+};
 
 const useData = <T>(url: string, options?: FetchOptions) => {
 	const { data, error } = useSWR<T>(
 		[url, options],
-		async (url: string, options: FetchOptions = {}) => {
-			const { data } = await axios(url, {
-				method: options.method || "get",
-				headers: {
-					"Content-Type": "application/json",
-					...options.headers,
-				},
-				data: options.data,
-			});
-			return data;
-		},
+		([url, options]) => fetcher<T>(url, options ?? {}),
 		{
-			revalidateOnFocus: false,
-			dedupingInterval: 60000,
-			...options,
+			revalidateOnFocus: false, // Disable revalidation on focus
+			dedupingInterval: 60000, // Cache data for 60 seconds
 		},
 	);
 
